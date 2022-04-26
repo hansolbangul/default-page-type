dcfb1cc29b739637ad4020a6f9ef7162b6dcf4be
hansolbangul <ruaenddl981028@naver.com>
2022-04-27 00:14:40 +0900

    header 위치

--- src/Router.tsx
+++ src/Router.tsx
@@ -1,9 +1,11 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import Header from './nav/Header';
 import Home from './routes/Home';
 
 function Router() {
   return (
     <BrowserRouter>
+      <Header></Header>
       <Routes>
         <Route path="/" element={<Home />}></Route>
       </Routes>
